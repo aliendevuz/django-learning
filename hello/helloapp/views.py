9777fa02addb12640db54f62871be3f9229358af
alien <aliendevuz@gmail.com>
2025-11-23 23:42:32 +0500

    Hello to Otabek

--- hello/helloapp/views.py
+++ hello/helloapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello(request):
-    return HttpResponse("Hello world!")
+    return HttpResponse("Hello Otabek Bro, Assalomu aleykum!")
